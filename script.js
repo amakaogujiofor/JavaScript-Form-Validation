563174beefe80a98ea0ee1efe6e622703c243c3c
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-12-10 21:40:25 +0100

    Added JavasScript logic for form validation

---
+++ script.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageCont = document.querySelector(".message-container");
+const message = document.getElementById("message");
+const button = document.getElementById("button");
+
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm() {
+  isValid = form.checkValidity();
+  message.textContent = "Please fill out all fields";
+
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Confirm both passwords";
+    button.textContent = "OOPS TRY AGAIN";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+    return;
+  }
+  //   Form Valid and Passwords Match
+  if (isValid && passwordsMatch) {
+    message.textContent = "Well done! You Did It";
+    button.textContent = "SUBMITTED";
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+    return;
+  }
+}
+
+function handleForm(e) {
+  e.preventDefault();
+  validateForm();
+}
+
+form.addEventListener("submit", handleForm);
